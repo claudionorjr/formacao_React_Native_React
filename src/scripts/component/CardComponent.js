31dd46ffdedfeb6ff395d75eb83ff965f489a0a8
Claudionor Silva <andregaragon@gmail.com>
2020-08-13 16:52:35 -0300

    iniciando state no react

--- src/scripts/component/CardComponent.js
+++ src/scripts/component/CardComponent.js
@@ -8,44 +8,49 @@ import BSN from 'bootstrap.native'
  * @description: Exporta por padrão um CardComponent.
  * 
  * @author Claudionor Silva <claudionor.junior1994@gmail.com>
- * @version 1.0.0
+ * @version 2.0.0
  * 
  * @param {NewsModel} props
  * @returns {CardComponent}
  */
-export default function CardComponent(props) {
-    var uSDateToBRDate = new USDateToBRDate(props.news.getPublishedAt())
-    const imgStyle = {maxHeight:'200px', width:'auto'}
-
-    return (
-        <div className="col mb-4">
-            <div className="card">
-                <img src={props.news.getUrlImage()} className="card-img-top" style={imgStyle} alt=""/>
-                <div className="card-body">
-                    <h5>{props.news.getTitle()}</h5>
-                    <p className="card-text">{props.news.getDescription()}</p>
-                    <p className="text-muted">Publicação: {uSDateToBRDate.date}</p>
-                    <p className="text-muted">Fonte: {props.news.getSource()}</p>
-                </div>
-                <div className="card-footer">
-                    <button className="btn btn-info btn-sm ml-2 mb-1" onClick={() => {
-                        let modal = document.getElementById('myModal')
-                        let title = document.getElementById('titleModal')
-                        let content = document.getElementById('contentModal')
-                        title.innerHTML = props.news.getTitle()
-                        content.innerHTML = props.news.getContent()
-                        new BSN.Modal(modal).toggle()
-                    }}>
-                        Ver Mais <i className="fa fa-plus"></i>
-                    </button>
-                    <button className="btn btn-light btn-sm ml-2 mb-1" onClick={() => {
-                        const noticeController = new NoticeController()
-                        noticeController.sendNoticeToModel(props.news)
-                    }}>
-                        Ler Depois <i className="fa fa-star-o"></i>
-                    </button>
+export default class CardComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {news: props.news}
+        this.uSDateToBRDate = new USDateToBRDate(props.news.getPublishedAt())
+    }
+    
+    render() {
+        return (
+            <div className="col mb-4">
+                <div className="card">
+                    <img src={this.state.news.getUrlImage()} className="card-img-top" style={{maxHeight:'200px', width:'auto'}} alt=""/>
+                    <div className="card-body">
+                        <h5>{this.state.news.getTitle()}</h5>
+                        <p className="card-text">{this.state.news.getDescription()}</p>
+                        <p className="text-muted">Publicação: {this.uSDateToBRDate.date}</p>
+                        <p className="text-muted">Fonte: {this.state.news.getSource()}</p>
+                    </div>
+                    <div className="card-footer">
+                        <button className="btn btn-info btn-sm ml-2 mb-1" onClick={() => {
+                            let modal = document.getElementById('myModal')
+                            let title = document.getElementById('titleModal')
+                            let content = document.getElementById('contentModal')
+                            title.innerHTML = this.state.news.getTitle()
+                            content.innerHTML = this.state.news.getContent()
+                            new BSN.Modal(modal).toggle()
+                        }}>
+                            Ver Mais <i className="fa fa-plus"></i>
+                        </button>
+                        <button className="btn btn-light btn-sm ml-2 mb-1" onClick={() => {
+                            const noticeController = new NoticeController()
+                            noticeController.sendNoticeToModel(this.state.news)
+                        }}>
+                            Ler Depois <i className="fa fa-star-o"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- src/scripts/component/CardFavoriteComponent.js
+++ src/scripts/component/CardFavoriteComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NoticeController from '../controller/NoticeController'
+import NoticeViewController from '../controller/NoticeViewController'
 import USDateToBRDate from './USDateToBRDate.js'
 import BSN from 'bootstrap.native'
 
@@ -13,39 +14,45 @@ import BSN from 'bootstrap.native'
  * @param {NewsModel} props
  * @returns {CardFavoriteComponent}
  */
-export default function CardFavoriteComponent(props) {
-    var uSDateToBRDate = new USDateToBRDate(props.news.getPublishedAt())
-    const imgStyle = {maxHeight:'200px', width:'auto'}
-
-    return (
-        <div className="col mb-4">
-            <div className="card">
-                <img src={props.news.getUrlImage()} className="card-img-top" style={imgStyle} alt=""/>
-                <div className="card-body">
-                    <h5>{props.news.getTitle()}</h5>
-                    <p className="card-text">{props.news.getDescription()}</p>
-                    <p className="text-muted">Publicação: {uSDateToBRDate.date}</p>
-                    <p className="text-muted">Fonte: {props.news.getSource()}</p>
-                </div>
-                <div className="card-footer">
-                    <button className="btn btn-info btn-sm ml-2 mb-1" onClick={() => {
-                        let modal = document.getElementById('myModal')
-                        let title = document.getElementById('titleModal')
-                        let content = document.getElementById('contentModal')
-                        title.innerHTML = props.news.getTitle()
-                        content.innerHTML = props.news.getContent()
-                        new BSN.Modal(modal).toggle()
-                    }}>
-                        Ver Mais <i className="fa fa-plus"></i>
-                    </button>
-                    <button className="btn btn-light btn-sm ml-2 mb-1" onClick={() => {
-                        const noticeController = new NoticeController()
-                        noticeController.deleteNotice(props.news.getTitle())
-                    }}>
-                        Remover Favorito <i className="fa fa-trash"></i>
-                    </button>
+export default class CardFavoriteComponent extends React.Component {
+    constructor(props) {
+        super(props)
+        this.noticeController = new NoticeController()
+        this.uSDateToBRDate = new USDateToBRDate(props.news.getPublishedAt())
+        this.state = {news: props.news}
+    }
+    
+    render() {
+        return (
+            <div className="col mb-4">
+                <div className="card">
+                    <img src={this.state.news.getUrlImage()} className="card-img-top" style={{maxHeight:'200px', width:'auto'}} alt=""/>
+                    <div className="card-body">
+                        <h5>{this.state.news.getTitle()}</h5>
+                        <p className="card-text">{this.state.news.getDescription()}</p>
+                        <p className="text-muted">Publicação: {this.uSDateToBRDate.date}</p>
+                        <p className="text-muted">Fonte: {this.state.news.getSource()}</p>
+                    </div>
+                    <div className="card-footer">
+                        <button className="btn btn-info btn-sm ml-2 mb-1" onClick={() => {
+                            let modal = document.getElementById('myModal')
+                            let title = document.getElementById('titleModal')
+                            let content = document.getElementById('contentModal')
+                            title.innerHTML = this.state.news.getTitle()
+                            content.innerHTML = this.state.news.getContent()
+                            new BSN.Modal(modal).toggle()
+                        }}>
+                            Ver Mais <i className="fa fa-plus"></i>
+                        </button>
+                        <button className="btn btn-light btn-sm ml-2 mb-1" onClick={() => {
+                            this.noticeController.deleteNotice(this.state.news.getTitle())
+                        }}>
+                            Remover Favorito <i className="fa fa-trash"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
